02b52ad18cbb3c3f387b656aaca53d0fccbee812
Wojciech Gancza <wgan@wgan.pl>
2022-11-13 13:39:37 +0100

    dev: refactoring of generator engine - code block placeholder also allow user defined code islands

--- source/meth.py
+++ source/meth.py
@@ -28,18 +28,21 @@ class target_content(file_lines):
      
 # file to store as result file. File is overwritten 
 # when store operation is called
-class result_file(list):
+class result_file:
+
+    def __init__(self, file_content):
+        self.lines = file_content
 
     def store(self, file_name):
         with open(file_name, "w", encoding = 'utf-8') as file:
-            for line in self:
+            for line in self.lines:
                 file.write(str(line) + "\n")
 
 # line content with additional information
 class line_content:
 
     def __init__(self, text):
-        self.text = text
+        self.text = str(text)
         
     def __str__(self):
         return self.text
@@ -49,54 +52,23 @@ class line_content:
       
     def is_persistent_block_end(self):
         return re.match("^\s*// -\^\^\^.*", self.text)
-       
-# transform template line and store it in output list
-class line_processor:
+        
+    def get_placeholder(self):
+        return re.search("[$][{][^}]*[}]", self.text)
 
-    def __init__(self, line, symbols):
-        self.text = str(line)
-        self.symbols = symbols
+# just a class to denote that the code lines should be 
+# threated as code, so no decoration between lines is performed
+# but the lines are interpreted
+class code_block:
     
-    # process replaces each occurance of ${ ... } by the value of python
-    # expression ... (calculated by eval function, so all known variables
-    # function objects and classes can be used. More placeholders in line are 
-    # allowed. 
-    def process(self, output):
-        if self.text == "":
-            output.append(self.text)
-        text = self.text
-        while text:
-            match = re.search("[$][{][^}]*[}]", text)
-            if not match:
-                output.append(text)
-                return
-            before = text[:match.start(0)]
-            after = text[match.end(0):]
-            expression = text[match.start(0)+2:match.end(0)-1]
-            value = eval(expression, self.symbols)
-            if type(value) is list:
-                separator = ","
-                if len(value) > 1:
-                    prefix = self._get_prefix(before)
-                    for scalar in value[:-1]:
-                        output.append(before + scalar + separator)
-                        before = prefix
-                text = before + str(value[-1]) + after
-            else:
-                text = before + str(value) + after         
-       
-    def _get_prefix(self, string):
-        result = ""
-        for char in string:
-            if char.isspace():
-                result = result + char
-            else:
-                result = result + ' '
-        return result
-    
-    
+    def __init__(self, list_od_lines):
+        self.list_of_lines = list_od_lines
+        
+    def get_list_of_lines(self):
+        return self.list_of_lines
+
 # just gives string from the list. One by obe
-class list_reader:
+class list_walker:
 
     def __init__(self, list_of_lines):
         self.lines_array = list_of_lines
@@ -113,8 +85,16 @@ class list_reader:
             line = self.lines_array[self.current_line]
             self.current_line = self.current_line + 1
             return line_content(line)
+            
+    # replace last read line by the given list. It also back line pointer
+    # to point to first line of added array
+    def push_back(self, lines):
+        self.lines_array = self.lines_array[:self.current_line-1] + lines + self.lines_array[self.current_line:]
+        self.current_line = self.current_line - 1
+        self.lines_count = len(self.lines_array)
 
     def go_to_line_with(self, content):
+        self.current_line = 0
         while not self.is_end():
             source_line = self.get_line()
             if str(content).strip() == str(source_line).strip():
@@ -126,21 +106,7 @@ class list_reader:
             if line.is_persistent_block_end():
                 break
 
-    def copy_till_block_end(self, output):
-        while not self.is_end():
-            line = self.get_line()
-            output.append(line)
-            if line.is_persistent_block_end():
-                break
-                
-    def process_till_block_end(self, output, symbols):
-        while not self.is_end():
-            line = line_content(self.get_line())
-            processor = line_processor(line, symbols)
-            processor.process(output)
-            if line.is_persistent_block_end():
-                        break
-
+# main functionality - generator creating output files
 class generator:
 
     def __init__(self):
@@ -152,28 +118,95 @@ class generator:
         
     def set_template(self, template_file_name):
         self.template_file_name = template_file_name
-        
+     
     def generate(self, result_file_name):
         template = file_lines(self.template_file_name)
+        transformed = self._transform(template)
         stored_file = target_content(result_file_name)
-        output = result_file()
-        template_lines = list_reader(template)
-        while not template_lines.is_end():
-            line = template_lines.get_line()
-            processor = line_processor(line, self.symbols)
-            processor.process(output)
-            if line.is_persistent_block_begin():
-                block_source = list_reader(stored_file)
-                block_source.go_to_line_with(line)
-                if not block_source.is_end():
-                    block_source.copy_till_block_end(output)
-                    template_lines.go_to_end_of_content_block()
-                else:
-                    template_lines.process_till_block_end(output, self.symbols)
+        output_content = self._combine_with_user_code(transformed, stored_file)  
+        output = result_file(output_content)
         output.store(result_file_name)
+        
+    def _transform(self, template):
+        walker = list_walker(template)
+        while not walker.is_end():
+            line = walker.get_line()
+            replacement = self._process_line(line)
+            if replacement:
+                walker.push_back(replacement)
+        return walker.lines_array
+ 
+    def _combine_with_user_code(self, transformd_template, existing_file):
+        generated = list_walker(transformd_template)
+        previous = list_walker(existing_file)
+        result = [ ]
+        while not generated.is_end():
+            line = generated.get_line()
+            result.append(str(line))
+            if line.is_persistent_block_begin():
+                previous.go_to_line_with(line)
+                if previous.is_end():
+                    self._copy_lines(generated, result)
+                else:
+                    self._copy_lines(previous, result)
+                    generated.go_to_end_of_content_block()
+        return result
+
+    # transfroms single line to output. Returns 'True' when the line result is 
+    # not the end of block and the content can be processed
+    def _process_line(self, line):
+        placeholder_match = line.get_placeholder()
+        line_text = str(line)
+        if placeholder_match:
+            before = line_text[:placeholder_match.start(0)]
+            after = line_text[placeholder_match.end(0):]
+            expression = line_text[placeholder_match.start(0)+2:placeholder_match.end(0)-1]
+            value = self._calculate_result(expression)
+            return self._combine_lines_from(before, value, after)   
+        return None
+    
+    # calculate result of expression to place in created text
+    def _calculate_result(self, expression):
+        return eval(expression, globals(), self.symbols)
+
+    def _combine_lines_from(self, before, value, after):
+        if type(value) is list:
+            list_len = len(value)
+            if list_len == 0:
+                return [ before+after ]
+            if list_len == 1:
+                return [ before+str(value[0])+after ]
+            else:
+                result = self._combine_lines_from(before, value[0], ",")
+                prefix = self._whitespace_text(before)
+                for scalar in value[1:-1]:
+                    result = result + self._combine_lines_from(prefix, scalar, ",")
+                result = result + self._combine_lines_from(prefix, value[-1], ",")
+                return result
+        elif type(value) is code_block:
+            result = [ ]
+            for line in value.get_list_of_lines():
+                result = result + self._combine_lines_from(before, line, after)
+            return result
+        else:
+            return [ before+str(value)+after ]
+            
+    def _whitespace_text(self, text):
+        return re.sub("\S", " ", text)
+    
+    def _copy_lines(self, source, target):
+        while not source.is_end():
+            line = source.get_line()
+            target.append(str(line))
+            if line.is_persistent_block_end():
+                return
     
 # ----------------------------------------------------------
 
+# library objects
+# some nice to use tools which could be usefull
+
+# class transforming string to identifier with variours notations
 class general_name:
 
     def __init__(self, name):
@@ -182,11 +215,35 @@ class general_name:
     def CamelCase(self):
         return "".join([x.capitalize() for x in self.bare_name])
         
+    def lowercaseCamel(self):
+        if len(self.bare_name):
+            first = self.bare_name[0].lower()
+            return first + "".join([x.capitalize() for x in self.bare_name[1:]])
+        else:
+            return ""
+        
+    def lowercase(self):
+        return "_".join([x.lower() for x in self.bare_name])
+        
+    def UPPERCASE(self):
+        return "_".join([x.upper() for x in self.bare_name])
+        
+# ----------------------------------------------------------
+
+def generate_some_code():
+    return code_block([ "void ${fname}", 
+                            "{", 
+                            "    // -vvv ${fname} user code", 
+                            "    // by default - do nothing",
+                            "    // -^^^ end of user code. do not modify",
+                            "}"])
 
 # Main functionality of source file generator. It 
 builder = generator()
 builder.set_template("../test/source.cpp")
 builder.define("name", general_name("hello World"))
-builder.define("enum_list", ["FIRST", "SECOND", "THIRD"])
+builder.define("fname", "a_function")
+builder.define("enum_list", ["FIRST", "SECOND", "THIRD", "AND_FINALLY_FORTH", "LAST"])
 builder.generate("../test/_generated/result.cpp")
 
+# ----------------------------------------------------------
