91ff3d612aa1a94468eab5ad60efd3086129bdc9
Wojciech Gancza <wgan@wgan.pl>
2022-11-13 15:42:06 +0100

    stb: Update of main function - to make it work.

--- source/MAIN.py
+++ source/MAIN.py
@@ -4,20 +4,26 @@ from meth import code_generator, code_block
 from tools import general_name
 
 # Main functionality of source file generator. It 
+template            = "../test/01-1-source_template.cpp"
+previous_result     = "../test/01-2-existing_content.cpp"
+expected_result     = "../test/01-3-expected_result.cpp"
+generated_result    = "../test/_generated/01-3-result.cpp"
+
 builder = code_generator(globals())
-
-def generate_some_code():
-    return code_block([ "void ${fname}", 
-                            "{", 
-                            "    // -vvv ${fname} user code", 
-                            "    // by default - do nothing",
-                            "    // -^^^ end of user code. do not modify",
-                            "}"])
-
-builder.set_template("../test/source.cpp")
+builder.set_template(template)
 builder.define("name", general_name("hello World"))
 builder.define("fname", "a_function")
 builder.define("enum_list", ["FIRST", "SECOND", "THIRD", "AND_FINALLY_FORTH", "LAST"])
-builder.generate("../test/_generated/result.cpp")
+
+def generate_some_code_with_placeholders_and_user_code_block():
+    return code_block([ 
+        "void ${fname}", 
+        "{", 
+        "    // -vvv ${fname} user code", 
+        "    // by default - do nothing",
+        "    // -^^^ end of user code. do not modify",
+        "}"])
+
+builder.generate(generated_result)
 
 # ----------------------------------------------------------
