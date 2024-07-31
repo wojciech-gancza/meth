8d55bc24c10db2239d9d9565d9e2cf674ead9816
Wojciech Gancza <wgan@wgan.pl>
2024-07-31 22:44:44 +0200

    Generation of common files used in various datatypes

--- data_types.py
+++ data_types.py
@@ -18,16 +18,20 @@ from inspect import currentframe, getmodule
 
 class data_types_generator:
 
-    def __init__(self, solution_root_folder, target_folder):
+    def __init__(self, solution_root_folder, target_folder, library_files_folder):
         self.generated_files = []
         self.solution_root_folder = file_name(solution_root_folder).get_full_file_name()
+        self.library_files_folder = file_name(library_files_folder)
         self.target_folder = file_name(target_folder)
         self.meth_directory = file_name(__file__ + "/..");
         self.template_folder = file_name(self.meth_directory.get_full_file_name() + "/templates/data_types")
         self.generator = code_generator( {} )
         self.generator.define("meth_directory",self.meth_directory.get_name_and_path_relative_to(self.solution_root_folder))
-        self.cpp_headers_folder = file_name(self.meth_directory.get_full_file_name() + "/cpp_headers")
-        self.generator.define("meth_headers_directory",self.cpp_headers_folder.get_name_and_path_relative_to(self.target_folder.get_full_file_name()))
+        self.need_integer_toolbox = False
+        self.need_string_toolbox = False
+        tools_relative_path = self._add_path_separator_if_needed(self.library_files_folder.get_name_and_path_relative_to(self.target_folder.get_full_file_name()))
+        tools_relative_path = self._add_path_separator_if_needed(tools_relative_path)
+        self.generator.define("tools_path", tools_relative_path)
 
     def create_integer_type(self, namespace, name, *, base_type="int", default_value=0, min_value=None, max_value=None, compareable=False, ordered=False):
         self._set_name(namespace, name)
@@ -38,13 +42,42 @@ class data_types_generator:
         self._set_trace()
         created_file_location = self._generate()
         self.generated_files.append(created_file_location)
+        self.need_integer_toolbox = True
 
-    def create_string_type(self, namespace, name, *, default_value = "", max_length=None, compareable=False, ordered=False):
+    def create_string_type(self, namespace, name, *, default_value = "", max_length=None, compareable=False, ordered=False, compare_strategy="Default"):
         self._set_name(namespace, name)
         self._set_type_properties(max_length, default_value)
-        self._set_comparision(compareable, ordered)
+        self._set_comparision(compareable, ordered, compare_strategy)
         self._set_template("string_type.h.template")
         self._set_trace()
+        created_file_location = self._generate()
+        self.generated_files.append(created_file_location)
+        self.need_string_toolbox = True
+
+    def add_toolbox_files(self):
+        need_common_tools = False
+        if self.need_string_toolbox:
+            need_common_tools = True
+        if self.need_string_toolbox:
+            self._create_library_file("meth_toolbox_strings.h", "_meth_toolbox_strings.template")
+            need_common_tools = True
+        if need_common_tools:
+            self._create_library_file("meth_toolbox_value_error.h", "_meth_toolbox_value_error.template")
+            self._create_library_file("meth_toolbox_deserialization_interface.h", "_meth_toolbox_deserialization_interface.template")
+            self._create_library_file("meth_toolbox_serialization_interface.h", "_meth_toolbox_serialization_interface.template")
+
+    def _add_path_separator_if_needed(self, path):
+        if not path:
+            return ""
+        if path[-1] != '/':
+            return path + '/'
+        return path
+
+    def _create_library_file(self, result_file_name, template_name):
+        self._set_trace()
+        self.output_file_name = file_name(self.library_files_folder.get_full_file_name() + "/" + result_file_name)
+        self.generator.define("file_name", self.output_file_name.get_name_and_path_relative_to(self.solution_root_folder))
+        self._set_template(template_name)
         created_file_location = self._generate()
         self.generated_files.append(created_file_location)
 
@@ -68,6 +101,12 @@ class data_types_generator:
         self.generator.define("max_length", max_length)
         self.default_value = default_value
         self.generator.define("default_value", str(default_value))
+        if max_length < 256:
+            self.generator.define("length_cathegory", "Short")
+        elif max_length < 65536:
+            self.generator.define("length_cathegory", "Long")
+        else:
+            self.generator.define("length_cathegory", "VeryLong")
 
     def _set_range(self, min_value, max_value):
         if not min_value is None and not max_value is None:
@@ -79,7 +118,7 @@ class data_types_generator:
             self.generator.define("max_value", 0)
             self.generator.define("range_controll", False)
 
-    def _set_comparision(self, compareable, ordered):
+    def _set_comparision(self, compareable, ordered, compare_strategy=None):
         if ordered:
             self.generator.define("compareable", True)
             self.generator.define("ordered", ordered)
@@ -95,6 +134,8 @@ class data_types_generator:
             self.generator.define("ordered", False)
             self.generator.define("compareable_flag_text", "false")
             self.generator.define("ordered_flag_text", "false")
+        if not compare_strategy is None:
+            self.generator.define("compare_strategy", str(compare_strategy))
 
     def _set_template(self, template_name):
         self.template_name = file_name(self.template_folder.get_full_file_name() + "/" + template_name)
